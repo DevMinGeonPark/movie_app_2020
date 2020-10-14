4cd737c20be8160979f4a26123b7570a93d5f86f
DevMInGeonPark <DevMinGeonPark@users.noreply.github.com>
2020-10-14 15:42:10 +0900

    Updates

---
+++ precache-manifest.30924c6147df308f342c5943f852d0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788d27c5685a72a3f86e236035786f8a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e36ef9774855b74a3052",
+    "url": "/movie_app_2020/static/css/main.9f4069f4.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36ef9774855b74a3052",
+    "url": "/movie_app_2020/static/js/main.14dc8d9d.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
